d3105c8a8dbb4bd3200ebc9e2f888112f31a105e
kalvin <VINIX@DEKSTOP-M61S7PN>
2023-03-24 23:12:22 +0700

    update title

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -4,7 +4,7 @@
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <title>Corona Admin</title>
+    <title>Login Panel</title>
     <!-- plugins:css -->
     <link rel="stylesheet" href="{{ asset('THEMEWAGON') }}/template/assets/vendors/mdi/css/materialdesignicons.min.css">
     <link rel="stylesheet" href="{{ asset('THEMEWAGON') }}/template/assets/vendors/css/vendor.bundle.base.css">
